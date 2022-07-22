a190088e794c186eed0b4b98ba0920956ea9cb8d
Nickle <xxnickles@gmail.com>
2022-07-21 22:45:59 -0400

    Fixing confirmation dialog

--- src/AnimeFeedManager.WebApp/State/Types.cs
+++ src/AnimeFeedManager.WebApp/State/Types.cs
@@ -5,4 +5,5 @@ public abstract record User();
 public record AnonymousUser() : User;
 
 public record AuthenticatedUser() : User;
-public record ApplicationUser(string Email, string Token) : User;
+
+public record ApplicationUser(string Email) : User;
