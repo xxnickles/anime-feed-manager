bbf253275a3ff8ec2b3a37d8c107e27dcd4f0e4c
nickle <xxnickles@gmail.com>
2022-08-22 14:41:09 -0400

    📐 Adding UI Filters

---
+++ src/AnimeFeedManager.WebApp/Filters.cs
@@ -0,0 +1,26 @@
+﻿using AnimeFeedManager.Common.Dto;
+
+namespace AnimeFeedManager.WebApp;
+
+public static class Filters
+{
+    public static bool Available(SimpleAnime anime) => anime.FeedInformation.Available;
+    public static bool NoAvailable(SimpleAnime anime) => !anime.FeedInformation.Available;
+    public static bool Completed(SimpleAnime anime) => anime.FeedInformation.Completed;
+
+    public static Func<SimpleAnime, bool> Subscribed(IEnumerable<string> subscribedTitles) =>
+        (SimpleAnime anime) => subscribedTitles.Contains(anime.FeedInformation.Title);
+
+    public static Func<SimpleAnime, bool> Interested(IEnumerable<string> interestedTitles) =>
+        (SimpleAnime anime) => interestedTitles.Contains(anime.Title);
+
+    public static IEnumerable<SimpleAnime> Filter(this IEnumerable<SimpleAnime> series,
+        IEnumerable<Func<SimpleAnime, bool>> filters)
+    {
+        return filters.Any()
+            ? filters
+                .Select(series.Where)
+                .SelectMany(x => x)
+            : series;
+    }
+}
