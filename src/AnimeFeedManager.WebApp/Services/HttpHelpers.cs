fdc38468c5459de53a80bb2aa3c35b366fd33b09
nickle <xxnickles@gmail.com>
2022-11-11 13:24:32 -0500

    🔼 Updating Mudblazor to dev3 (native support for net7.0) 🩹 Removing traces

--- src/AnimeFeedManager.WebApp/Services/HttpHelpers.cs
+++ src/AnimeFeedManager.WebApp/Services/HttpHelpers.cs
@@ -10,16 +10,16 @@ public static class HttpHelpers
 {
     private record ProblemDetails(string Type, string Title, string Instance, HttpStatusCode Status,
         ImmutableDictionary<string, string[]>? Errors, string? Detail);
-    
+
     public static async Task<ImmutableList<T>> MapToList<T>(this HttpResponseMessage response)
     {
         var result = await response.MapToObject(Enumerable.Empty<T>());
         return result.ToImmutableList();
     }
-    
+
     public static async Task<T> MapToObject<T>(this HttpResponseMessage response, T defaultValue)
     {
-        if(response.StatusCode == HttpStatusCode.NoContent)
+        if (response.StatusCode == HttpStatusCode.NoContent)
             return defaultValue;
         var result = await response.Content.ReadFromJsonAsync<T>();
         return result ?? defaultValue;
@@ -32,7 +32,6 @@ public static class HttpHelpers
     /// <returns></returns>
     public static async Task CheckForProblemDetails(this HttpResponseMessage response)
     {
-        Console.WriteLine($"error response? {response.StatusCode >= HttpStatusCode.BadRequest}");
         // Only check for errors!
         if (response.StatusCode >= HttpStatusCode.BadRequest)
         {
@@ -50,11 +49,9 @@ public static class HttpHelpers
                 Console.WriteLine($"has detail {bodyObject.Detail}");
                 throw new HttpProblemDetailsException(bodyObject.Title, bodyObject.Detail, response.StatusCode);
             }
-              
-
         }
-        
+
         // Default behavior
         response.EnsureSuccessStatusCode();
-    } 
+    }
 }
