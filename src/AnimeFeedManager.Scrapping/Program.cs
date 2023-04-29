4b83d4b19c016ff7f5f31cad66234a093044fe5d
nickle <xxnickles@gmail.com>
2023-04-28 21:30:42 -0400

    Scaffolding

---
+++ src/AnimeFeedManager.Scrapping/Program.cs
@@ -0,0 +1,7 @@
+using AnimeFeedManager.Scrapping;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+    .Build();
+
+host.Run();
