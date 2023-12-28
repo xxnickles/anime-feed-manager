f1b40728ad2aac619886b246fed7260cae8822bb
nickle <xxnickles@gmail.com>
2023-12-27 23:26:31 -0500

    🛠️ Completing basic authentication/authorization

--- src/AnimeFeedManager.Web/Program.cs
+++ src/AnimeFeedManager.Web/Program.cs
@@ -1,8 +1,11 @@
 using AnimeFeedManager.Web.Bootstrapping;
 using AnimeFeedManager.Web.Features;
+using AnimeFeedManager.Web.Features.Security;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Server;
 using Microsoft.AspNetCore.Components.Web;
-using TvEndpoints = AnimeFeedManager.Web.Features.Tv.Endpoints; 
+using TvEndpoints = AnimeFeedManager.Web.Features.Tv.Endpoints;
 using AdminEndpoints = AnimeFeedManager.Web.Features.Admin.Endpoints;
 using SecurityEndpoints = AnimeFeedManager.Web.Features.Security.Endpoints;
 using Passwordless.Net;
@@ -12,6 +15,17 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents();
 
+builder.Services.AddCascadingAuthenticationState();
+builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+        options.LogoutPath = "/logout";
+    });
+
 // bind section Passwordless to the object PassworlessOptions
 builder.Services.Configure<PasswordlessOptions>(builder.Configuration.GetSection("Passwordless"));
 // Add Passworless
@@ -20,14 +34,9 @@ builder.Services.AddPasswordlessSdk(options =>
     builder.Configuration.GetRequiredSection("Passwordless").Bind(options);
 });
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.LoginPath = "/login";
-    });
+// builder.Services.AddHttpContextAccessor();
+// builder.Services.AddScoped<AuthenticationStateProvider, AppAuthenticationStateProvider>();
 
-builder.Services.AddAuthorization();
-// builder.Services.AddCascadingAuthenticationState();
 // Add the renderer and wrapper to services
 builder.Services.AddScoped<HtmlRenderer>();
 builder.Services.AddScoped<BlazorRenderer>();
