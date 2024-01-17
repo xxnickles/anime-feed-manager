ef7208882acd5541fd690de4a9b26e112ffed401
nickle <xxnickles@gmail.com>
2024-01-17 17:21:39 -0500

    🛠️ Fixing the selector to work with user device settings 🔼 Updating dependencies

---
+++ src/AnimeFeedManager.Web/Features/Common/ThemeSelector.razor.js
@@ -0,0 +1,28 @@
+﻿function themeDetector() {
+    return {
+        theme: 'dim',
+        isDark: false,
+        init() {
+            const matchMediaDark = window.matchMedia('(prefers-color-scheme: dark)');
+            const matchMediaLight = window.matchMedia('(prefers-color-scheme: light)');
+
+            // set theme based on initial state
+            this.theme = matchMediaDark.matches ? 'emerald' : 'dim';
+            this.isDark = matchMediaDark.matches;
+
+            matchMediaDark.addEventListener('change',(e) => {
+                if (e.matches) {
+                    this.theme = 'emerald';
+                    this.isDark = true;
+                }
+            });
+
+            matchMediaLight.addEventListener('change',(e) => {
+                if (e.matches) {
+                    this.theme = 'dim';
+                    this.isDark = false;
+                }
+            });
+        },
+    };
+}
