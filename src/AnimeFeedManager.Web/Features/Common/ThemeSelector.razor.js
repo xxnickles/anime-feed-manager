0816ce39d7c6ae305157289f8c60be9b59cc36d7
nickle <xxnickles@gmail.com>
2024-01-20 00:28:53 -0500

    🐛 Deactivating theme selector as need to be designed to be work wit SSR

--- src/AnimeFeedManager.Web/Features/Common/ThemeSelector.razor.js
+++ src/AnimeFeedManager.Web/Features/Common/ThemeSelector.razor.js
@@ -1,28 +1,54 @@
-﻿function themeDetector() {
+﻿const availableThemes = {
+    'dark': 'dim',
+    'light': 'emerald'
+}
+
+function getOppositeTheme(theme) {
+    return theme === availableThemes['light'] ? availableThemes['dark'] : availableThemes['light'];
+}
+
+function themeDetector() {
     return {
-        theme: 'dim',
+        selectedTheme: availableThemes['light'],
+        targetTheme: availableThemes['dark'],
         isDark: false,
+        themeChanged(event) {
+            this.selectedTheme = getOppositeTheme(this.selectedTheme);
+            console.log("new theme", this.selectedTheme)
+            localStorage.setItem('user-theme', this.selectedTheme);
+        },
         init() {
             const matchMediaDark = window.matchMedia('(prefers-color-scheme: dark)');
             const matchMediaLight = window.matchMedia('(prefers-color-scheme: light)');
 
-            // set theme based on initial state
-            this.theme = matchMediaDark.matches ? 'emerald' : 'dim';
-            this.isDark = matchMediaDark.matches;
+            if (!localStorage.getItem('user-theme')) {
+                // set theme based on initial state
+                this.targetTheme = matchMediaDark.matches ? availableThemes['light'] : availableThemes['dark'];
+                this.isDark = matchMediaDark.matches;
+                this.selectedTheme = getOppositeTheme(this.targetTheme);
+            } else {
+                this.selectedTheme = localStorage.getItem('user-theme');
+                this.targetTheme = getOppositeTheme(this.selectedTheme);
+                this.isDark = this.targetTheme === availableThemes['dark'];
+                document.querySelector('html').setAttribute('data-theme', this.selectedTheme);
+            }
 
-            matchMediaDark.addEventListener('change',(e) => {
+
+            matchMediaDark.addEventListener('change', (e) => {
+
                 if (e.matches) {
-                    this.theme = 'emerald';
+                    this.targetTheme = availableThemes['light'];
                     this.isDark = true;
+
                 }
             });
 
-            matchMediaLight.addEventListener('change',(e) => {
+            matchMediaLight.addEventListener('change', (e) => {
                 if (e.matches) {
-                    this.theme = 'dim';
+                    this.targetTheme = availableThemes['dark'];
                     this.isDark = false;
                 }
             });
-        },
+        }
     };
 }
