b79cc61bc041691b07ad9217e759a1d080633e9c
nickle <xxnickles@gmail.com>
2024-07-04 18:41:55 -0400

    🗑️ Removing Blazor Webassembly Project

--- src/AnimeFeedManager.WebApp.Authentication/Models/AuthData.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT License. See LICENSE in the project root for license information.
-
-namespace AnimeFeedManager.WebApp.Authentication.Models;
-
-public class AuthenticationData
-{
-    public ClientPrincipal ClientPrincipal { get; set; }
-}
--- src/AnimeFeedManager.WebApp/Exceptions/HttpProblemDetailsException.cs
+++
@@ -1,9 +0,0 @@
-using System.Net;
-
-namespace AnimeFeedManager.WebApp.Exceptions;
-
-public class HttpProblemDetailsException(string message, string detail, HttpStatusCode statusCode)
-    : HttpRequestException(message, null, statusCode)
-{
-    public string Detail { get; } = detail;
-}
