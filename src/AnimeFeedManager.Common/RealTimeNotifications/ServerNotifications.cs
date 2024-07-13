410c17c089c25f3666576d80216a919f90159d8c
nickle <xxnickles@gmail.com>
2024-07-13 19:49:25 -0400

    🛠️ Improving visibility of ovas and series with links 📐 Adding feed update notifications 🧹 Cleanups

--- src/AnimeFeedManager.Common/RealTimeNotifications/ServerNotifications.cs
+++ src/AnimeFeedManager.Common/RealTimeNotifications/ServerNotifications.cs
@@ -5,5 +5,6 @@ public static class ServerNotifications
 {
     public const string SeasonProcess = "seasonprocess";
     public const string TitleUpdate = "titleupdate";
+    public const string FeedUpdates = "feedUpdates";
     public const string ImageUpdate = "imageupdate";
 }
