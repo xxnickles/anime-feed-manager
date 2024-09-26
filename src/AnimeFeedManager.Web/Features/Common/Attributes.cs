a11961a1774825406193cd42955f40f41fee9c21
nickle <xxnickles@gmail.com>
2024-09-25 20:46:53 -0400

    📐 Adding initial client side filtering for Ovas and Movies
    🔼 Updating dependencies

---
+++ src/AnimeFeedManager.Web/Features/Common/Attributes.cs
@@ -0,0 +1,8 @@
+﻿namespace AnimeFeedManager.Web.Features.Common;
+
+internal static class Attributes
+{
+    internal static readonly KeyValuePair<string, object> Available = new("data-available", "true");
+    internal static readonly KeyValuePair<string, object> Susbcribed = new("data-subscribed", "true");
+    internal static readonly KeyValuePair<string, object> HasFeed = new("data-has-feed", "true");
+}
