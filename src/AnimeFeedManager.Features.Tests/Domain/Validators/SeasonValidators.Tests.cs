18597c62eb81bcf658c05e35afa58a4311d8d961
nickle <xxnickles@gmail.com>
2025-01-31 21:32:57 -0500

    🔼 Updating dependencies

--- src/AnimeFeedManager.Features.Tests/Domain/Validators/SeasonValidators.Tests.cs
+++ src/AnimeFeedManager.Features.Tests/Domain/Validators/SeasonValidators.Tests.cs
@@ -6,13 +6,13 @@ namespace AnimeFeedManager.Features.Tests.Domain.Validators;
 public class SeasonValidatorsTests
 {
     [Theory]
-    [InlineData("Summer", 0, new []{ "Year" })]
-    [InlineData("Winter", 1, new []{ "Year" })]
-    [InlineData("Autumn", 1999, new []{ "Year" })]
-    [InlineData("Fall", 0, new []{ "Year" })]
-    [InlineData("Spring", 0, new []{ "Year" })]
-    [InlineData("bad", 2023, new []{ "Season" })]
-    [InlineData("bad", 10, new []{ "Year", "Season" })]
+    [InlineData("Summer", 0, new[] {"Year"})]
+    [InlineData("Winter", 1, new[] {"Year"})]
+    [InlineData("Autumn", 1999, new[] {"Year"})]
+    [InlineData("Fall", 0, new[] {"Year"})]
+    [InlineData("Spring", 0, new[] {"Year"})]
+    [InlineData("bad", 2023, new[] {"Season"})]
+    [InlineData("bad", 10, new[] {"Year","Season"})]
     public void Should_Invalidate_Wrong_Inputs(string season, ushort year, string[] expectedErrorFields)
     {
         var sut = SeasonValidators.Parse(season, year);
@@ -21,8 +21,9 @@ public class SeasonValidatorsTests
             e =>
             {
                 Assert.IsType<ValidationErrors>(e);
-                var errors = (ValidationErrors)e;
-                Assert.Equal(expectedErrorFields, errors.Errors.Keys);
+                var errors = (ValidationErrors) e;
+                Assert.Equal(expectedErrorFields.Length, errors.Errors.Keys.Length());
+                Assert.True(expectedErrorFields.All(errors.Errors.Keys.Contains));
             });
     }
 
@@ -36,23 +37,23 @@ public class SeasonValidatorsTests
             e =>
             {
                 Assert.IsType<ValidationErrors>(e);
-                var errors = (ValidationErrors)e;
+                var errors = (ValidationErrors) e;
                 Assert.Equal(["Year"], errors.Errors.Keys);
             });
     }
-    
+
     [Fact]
     public void Season_Validator_Should_Respect_Upper_Boundary()
     {
         // Cannot ve lower than 20009
-        var sut = SeasonValidators.Parse("Summer", (ushort)(DateTime.Now.Year + 2));
+        var sut = SeasonValidators.Parse("Summer", (ushort) (DateTime.Now.Year + 2));
         // or larger than the next year
         sut.Match(
             _ => Assert.Fail("Should not be a positive result"),
             e =>
             {
                 Assert.IsType<ValidationErrors>(e);
-                var errors = (ValidationErrors)e;
+                var errors = (ValidationErrors) e;
                 Assert.Equal(["Year"], errors.Errors.Keys);
             });
     }
