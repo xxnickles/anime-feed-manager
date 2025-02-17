3fa212d706ec121b9f19d23fb749552ff9ff4715
nickle <xxnickles@gmail.com>
2025-02-17 10:51:34 -0500

    🔼 Updating DaisyUI

--- src/AnimeFeedManager.Web/tailwind.config.js
+++ src/AnimeFeedManager.Web/tailwind.config.js
@@ -1,24 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-    content: ["./**/*.{razor,cshtml}", "../AnimeFeedManager.Web.BlazorComponents/**/*.{razor,cshtml}"],
-    theme:{
-      extend: {
-          fontFamily: {
-              'sans': ['"SF Mono"', ...defaultTheme.fontFamily.sans],
-              'logo' : ['"Prettier Script"', 'sans-serif']
-          }
-      }  
-    },
-    daisyui: {
-        themes: ["nord", "dim"],
-        darkTheme: "dim"        
-    },
-    plugins: [
-        require('@tailwindcss/typography'),
-        require("daisyui"),
-    ],
+    content: ["./**/*.{razor,cshtml}", "../AnimeFeedManager.Web.BlazorComponents/**/*.{razor,cshtml}"],   
     safelist: [
         'stroke-success',
         'stroke-error',
